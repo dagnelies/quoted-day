66e1f2c70927bbb23c40e420b9b5721a759ef3da
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-11-25 12:46:42 +0000

    ...

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -4,8 +4,8 @@ export const prerender = false;
 export async function GET({ locals, params }) {
   const currentMillis = Date.now();
   const randomIndex = Math.floor(5400 * Math.random());
-  const quoteRaw = await locals.runtime.env.QUOTES.get("quote-00003");
-
+  //const quoteRaw = await locals.runtime.env.QUOTES.get("quote-00003");
+  const quoteRaw = await QUOTES.get(`quote-${String(randomIndex).padStart(5, '0')}`);
   const quote = JSON.parse(quoteRaw);
     
 
