f96801216a4538f68506110f7700a4dfbb37f187
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-11-25 12:45:31 +0000

    ...

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -2,23 +2,17 @@
 export const prerender = false;
 
 export async function GET({ locals, params }) {
-  const quotes = [
-      "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-      "In the middle of every difficulty lies opportunity. - Albert Einstein",
-      "What you get by achieving your goals is not as important as what you become by achieving your goals. - Zig Ziglar",
-      "Believe you can and you're halfway there. - Theodore Roosevelt",
-      "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-  ];
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const quote1 = quotes[randomIndex];
+  const currentMillis = Date.now();
+  const randomIndex = Math.floor(5400 * Math.random());
+  const quoteRaw = await locals.runtime.env.QUOTES.get("quote-00003");
 
-  const quote2 = await locals.runtime.env.QUOTES?.get("quote-00003");
-
+  const quote = JSON.parse(quoteRaw);
+    
 
   return new Response(JSON.stringify(
     {
-      quote1,
-      quote2
+      ...quote,
+      elapsedServerTime: Date.now() - currentMillis,
     }), {
     status: 200,
     headers: {
