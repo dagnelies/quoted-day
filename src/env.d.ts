a47e2d64d9cd9fb495e315ee77018eacbd2452c5
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-01 14:32:29 +0000

    ...

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,11 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
   interface Locals extends Runtime {
-    QUOTES :KVNamespace
+    // Add binding to KV namespace QUOTES
+    runtime: {
+      env: {
+        QUOTES: KVNamespace;
+      }  
+    }
   }
 }
--- src/pages/api/quote.ts
+++ src/pages/api/quote.ts
@@ -1,15 +1,20 @@
+import type { APIContext } from "astro";
 
 export const prerender = false;
 
-export async function GET({ locals, params }) {
-  const t = Date.now();
-
+export async function getQuote(context :APIContext)  : Promise<{ text: string; author: string }> {
   const randomIndex = Math.floor(5400 * Math.random());
   //const quoteRaw = await locals.runtime.env.QUOTES.get("quote-00003");
-  const quoteRaw = await locals.runtime.env.QUOTES.get(`quote-${String(randomIndex).padStart(5, '0')}`);
+  const quoteRaw = await context.locals.runtime.env.QUOTES.get(`quote-${String(randomIndex).padStart(5, '0')}`) ?? '{}';
   const quote = JSON.parse(quoteRaw);
-    
+  return quote;
+}
 
+export async function GET(context :APIContext) {
+  const t = Date.now();
+
+  const quote = await getQuote(context);
+    
   return new Response(JSON.stringify(
     {
       ...quote,
