1454c980abba9c25e08c16f0a8b43fc900274e29
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-10 10:56:42 +0000

    ...

--- src/pages/api/quote.ts
+++ src/pages/api/quote.ts
@@ -1,7 +1,7 @@
 import type { APIContext } from "astro";
 
-export async function getQuote(context :APIContext)  : Promise<{ text: string; author: string }> {
-  const randomIndex = Math.floor(5400 * Math.random());
+export async function getQuote(context :APIContext, num?: number)  : Promise<{ text: string; author: string }> {
+  const randomIndex = num ?? Math.floor(5420 * Math.random());
   const quotesKV = context.locals.runtime.env.QUOTES;
   await quotesKV.put('last-request', new Date().toISOString());
   const quoteRaw = await quotesKV.get(`quote-${String(randomIndex).padStart(5, '0')}`) ?? '{}';
