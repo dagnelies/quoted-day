a15ddc0e42599a1d63a2157f0be6b2b413099944
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-10 10:37:20 +0000

    ...

--- src/scripts/buildQuotesKV.ts
+++ src/scripts/buildQuotesKV.ts
@@ -4,12 +4,14 @@
 import fs from 'fs';
 
 const quotes = JSON.parse(fs.readFileSync('src/scripts/quotes.json', 'utf-8'));
-const kvEntries: Record<string, string> = {};
+const kvList :Array<{key: string, value: string}> = [];
 
 quotes.forEach((quote: { quoteText: string; quoteAuthor: string }, index: number) => {
-  const key = `quote-${String(index).padStart(5, '0')}`;
-  kvEntries[key] = JSON.stringify({ text: quote.quoteText, author: quote.quoteAuthor });
+  kvList.push({
+    key: `quote-${String(index).padStart(5, '0')}`,
+    value: JSON.stringify({text: quote.quoteText, author: quote.quoteAuthor})
+  });
 });
 
-fs.writeFileSync('src/scripts/quotesKV.json', JSON.stringify(kvEntries, null, 2), 'utf-8');
+fs.writeFileSync('src/scripts/quotesKV.json', JSON.stringify(kvList, null, 2), 'utf-8');
 console.log('quotesKV.json has been created with key-value pairs for Cloudflare KV.');
