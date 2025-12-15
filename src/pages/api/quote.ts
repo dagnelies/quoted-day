660d51ec69564ea1fdff29687eed9b70950ca1b9
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-15 16:38:58 +0000

    let's cache it for a year!

--- src/pages/api/quote.ts
+++ src/pages/api/quote.ts
@@ -3,12 +3,8 @@ import type { APIContext } from "astro";
 export async function getQuote(context :APIContext, num?: number)  : Promise<{ text: string; author: string }> {
   const randomIndex = num ?? Math.floor(5420 * Math.random());
   const quotesKV = context.locals.runtime.env.QUOTES;
-  const quoteRaw = await quotesKV.get(`quote-${String(randomIndex).padStart(5, '0')}`);
-  /*const quoteRaw = await quotesKV.getWithMetadata(`quote-${String(randomIndex).padStart(5, '0')}`, {
-    cacheTtl: 86400, // cache it for a day by default
-  });*/
-  const quote = JSON.parse(quoteRaw);
-  return quote;
+  const quote = await quotesKV.get(`quote-${String(randomIndex).padStart(5, '0')}`, { type: "json", cacheTtl: 365 * 24 * 3600 });
+  return quote as { text: string; author: string };
 }
 
 export async function GET(context :APIContext) {
