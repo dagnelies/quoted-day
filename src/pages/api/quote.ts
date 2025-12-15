90d54ac68e650925b200a2d7e80d3f0a5dfb8d29
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-15 16:33:39 +0000

    ...

--- src/pages/api/quote.ts
+++ src/pages/api/quote.ts
@@ -3,9 +3,10 @@ import type { APIContext } from "astro";
 export async function getQuote(context :APIContext, num?: number)  : Promise<{ text: string; author: string }> {
   const randomIndex = num ?? Math.floor(5420 * Math.random());
   const quotesKV = context.locals.runtime.env.QUOTES;
-  const quoteRaw = await quotesKV.getWithMetadata(`quote-${String(randomIndex).padStart(5, '0')}`, {
+  const quoteRaw = await quotesKV.get(`quote-${String(randomIndex).padStart(5, '0')}`);
+  /*const quoteRaw = await quotesKV.getWithMetadata(`quote-${String(randomIndex).padStart(5, '0')}`, {
     cacheTtl: 86400, // cache it for a day by default
-  });
+  });*/
   const quote = JSON.parse(quoteRaw);
   return quote;
 }
