a35a9f907e83c8ecba4e126bd058e3f4fa3434ae
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-18 14:19:48 +0000

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-//import mdx from "@astrojs/mdx";
-//import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
