1d2f8009ab5e124bd93614f6a01d336c1d6d7f45
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2025-12-29 17:17:33 +0000

    ...

--- src/pages/api/bump-counter.ts
+++ src/pages/api/bump-counter.ts
@@ -4,12 +4,13 @@ import type { APIContext } from "astro";
 export async function GET(context :APIContext) {
   
   const quotesKV = context.locals.runtime.env.QUOTES;
-  const counterRaw = await quotesKV.get(`counter`, { type: "text", cacheTtl: 365 * 24 * 3600 });
+  const counterRaw = await quotesKV.get(`counter-${context.clientAddress}`, { type: "text", cacheTtl: 365 * 24 * 3600 });
   const counter = Number(counterRaw ?? "0") + 1;
-  await quotesKV.put(`counter`, counter.toString());
-  
+  await quotesKV.put(`counter-${context.clientAddress}`, counter.toString());
+
   return new Response(JSON.stringify(
     {
+      from: context.clientAddress,
       time: new Date(Date.now()).toISOString(),
       counter
     }), {
